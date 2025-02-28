b4ad625df5f5774fd8160625035192717996e372
sunny834 <sunnyvlog10@gmail.com>
2025-02-28 16:35:25 +0530

    Update

---
+++ src/app/data.js
@@ -0,0 +1,123 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Toypebble",
+    description: "A subscription based partial ecommerce site for toys",
+    date: "2024-07-02",
+    demoLink: "https://toypebble.com/",
+  },
+  {
+    id: 2,
+    name: "Leons",
+    description: "A burger king website",
+    date: "2024-05-22",
+    demoLink: "http://leons.in/",
+  },
+  {
+    id: 3,
+    name: "MontiAngels International School",
+    description: "A website for an international school",
+    date: "2024-04-25",
+    demoLink: "https://www.montiangelsinternationalschool.com/",
+  },
+  {
+    id: 4,
+    name: "RP Infocare",
+    description: "A service based company's website",
+    date: "2024-05-08",
+    demoLink: "https://www.rpinfocare.com/",
+  },
+  {
+    id: 5,
+    name: "Crescent Workspace",
+    description: "Redefining work culture",
+    date: "2024-03-28",
+    demoLink: "https://crescentworkspace.in/",
+  },
+  {
+    id: 6,
+    name: "Torosand",
+    description: "Build with trust",
+    date: "2024-04-02",
+    demoLink: "https://www.torosand.in/",
+  },
+  {
+    id: 7,
+    name: "Unicorp Elevators",
+    description: "Travel Comfort",
+    date: "2024-03-19",
+    demoLink: "https://www.unicorpelevators.in/",
+  },
+  {
+    id: 8,
+    name: "Little Screens",
+    description: "A theater booking application",
+    date: "2024-12-18",
+    demoLink: "https://thelittlescreens.com/",
+  },
+  {
+    id: 9,
+    name: "Advi Groups",
+    description: "An IT sector electronic and manufacturing company's application",
+    date: "2025-02-03",
+    demoLink: "https://advigroups.org/",
+  },
+  {
+    id: 10,
+    name: "Yashoda Trust",
+    description: "A donation application for children help and study",
+    date: "2025-02-10",
+    demoLink: "https://mindfulmoments.example.com",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/Nandita-Kumari",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/nandita-kumari-999995216/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://x.com/NANDITAKUM93833?t=rcKHkYQuk4WA3DWphMeBXg&s=08",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
---
+++ src/components/about/index.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+import Link from "next/link";
+
+const AboutDetails = () => {
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+        <ItemLayout
+          className={
+            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+          }
+        >
+          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
+            Architect of Enchantment
+          </h2>
+          <p className="font-light  text-xs sm:text-sm md:text-base   ">
+          My name is Nandita Kumari, and my portfolio is a representation of all that i have learned and accomplished as a science and engineering student. I love designing websites and looking for more opportunities. While you are visiting my portfolio, you get to learn a bit about me, my goals as they relate to the immediate future and many years down the road, and some of my many life lessons that i have learned outside of the classroom that have shaped me into the person I am today.
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            20+ <sub className="font-semibold text-base">Projects</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            2+{" "}
+            <sub className="font-semibold text-base">years of experience</sub>
+          </p>
+        </ItemLayout>
+
+        {/* <ItemLayout
+          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
+        >
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="Nandita"
+            loading="lazy"
+          />
+        </ItemLayout> */}
+
+        {/* <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout> */}
+
+        <ItemLayout className={"col-span-full"}>
+          <img
+            className="w-full h-auto"
+            src={`https://skillicons.dev/icons?i=cpp,react,angular,mongodb,aws,babel,bootstrap,cloudflare,css,d3,figma,firebase,git,github,graphql,html,js,jquery,kubernetes,nextjs,nodejs,npm,redux,sass,tailwind,threejs,vite,vscode`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        {/* <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=codebucks27&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout> */}
+
+        {/* <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <Link
+            href="https://github.com/codebucks27/Nextjs-contentlayer-blog"
+            target="_blank"
+            className="w-full"
+          >
+            <img
+              className="w-full h-auto"
+              src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/pin/?username=codebucks27&repo=Nextjs-contentlayer-blog&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
+              alt="CodeBucks"
+              loading="lazy"
+            />
+          </Link>
+        </ItemLayout> */}
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
